2536f51846d0d5162acf8d56725ee3e27d696ee2
Sidharth Menon <sidharth@startupmission.in>
2025-08-27 04:37:50 +0000

    changes

--- config/logging.php
+++ config/logging.php
@@ -60,14 +60,14 @@ return [
 
         'single' => [
             'driver' => 'single',
-            'path' => getcwd().'logs/laravel.log',
+            'path' => './logs/laravel.log',
             'level' => env('LOG_LEVEL', 'debug'),
             'replace_placeholders' => true,
         ],
 
         'daily' => [
             'driver' => 'daily',
-            'path' => getcwd().'logs/laravel.log',
+            'path' => './logs/laravel.log',
             'level' => env('LOG_LEVEL', 'debug'),
             'days' => env('LOG_DAILY_DAYS', 14),
             'replace_placeholders' => true,
@@ -124,7 +124,7 @@ return [
         ],
 
         'emergency' => [
-            'path' => getcwd().'logs/laravel.log',
+            'path' => './logs/laravel.log',
         ],
 
     ],
