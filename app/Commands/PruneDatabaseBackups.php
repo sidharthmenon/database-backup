1b5a125df57b025969add98d51c2d1ae6f68b90f
Sidharth Menon <sidharth@startupmission.in>
2025-08-27 05:02:30 +0000

    changes

--- app/Commands/PruneDatabaseBackups.php
+++ app/Commands/PruneDatabaseBackups.php
@@ -119,6 +119,6 @@ class PruneDatabaseBackups extends Command
      */
     public function schedule(Schedule $schedule): void
     {
-        $schedule->command(static::class)->dailyAt("06:00");
+        // $schedule->command(static::class)->dailyAt("06:00");
     }
 }
