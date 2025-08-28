704b511c0fa32e9fd1b10a90097659c6c597e7ef
Sidharth Menon <sidharth@startupmission.in>
2025-08-28 16:19:58 +0000

    chag

--- database/migrations/2025_08_28_161851_backup-server-migration.php
+++ database/migrations/2025_08_28_161851_backup-server-migration.php
@@ -4,8 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-new class extends Migration
+return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::create('backup_server_destinations', function (Blueprint $table) {
@@ -108,5 +111,13 @@ new class extends Migration
             $table->longText('message');
             $table->timestamps();
         });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
     }
 };
