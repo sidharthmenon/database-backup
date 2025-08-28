9ca7a1da6ae12040bd1363a3838848b906fdc16b
Sidharth Menon <sidharth@startupmission.in>
2025-08-28 16:13:45 +0000

    changes

--- database/migrations/2025_08_27_094555_create_backup_server_tables.php
+++ database/migrations/2025_08_27_094555_create_backup_server_tables.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBackupServerTables extends Migration
+new class extends Migration
 {
     public function up(): void
     {
@@ -109,4 +109,4 @@ class CreateBackupServerTables extends Migration
             $table->timestamps();
         });
     }
-}
+};
